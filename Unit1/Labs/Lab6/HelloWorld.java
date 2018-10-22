2879d64284ed93fca7eca43941d13bdd390e4ccc
unknown <miqui98e@gmail.com>
2018-10-22 12:17:38 -0500

    latest lab

---
+++ Unit1/Labs/Lab6/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+	//se guarda al file como HelloWorld.java, igual que a la clase
+	public static void main(String[] args){
+		System.out.print("Hello OOP World, from Miqui");
+	}
+}
